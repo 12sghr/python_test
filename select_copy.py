d048aba8aa4e04cb6511fd1a76161c1bb87d587b
sghr12 <takezone103@gmail.com>
2018-06-25 17:59:05 +0900

    展開先フォルダの自動生成と最終的にいらないものを削除するように

--- select_copy.py
+++ select_copy.py
@@ -3,11 +3,17 @@ import os, shutil, zipfile
 shutil.copy('101_ObjectCategories.zip', 'copied_file.zip')
 
 data = zipfile.ZipFile('copied_file.zip')
+if 'extract' not in os.listdir():
+    os.mkdir('extract')
 data.extractall('extract')
 data.close()
+
+if 'res' not in os.listdir():
+    os.mkdir('res')
 
 for foldername, subfolders, filenames in os.walk('extract/101_ObjectCategories'):
     for filename in filenames:
         shutil.move(foldername + '/' + filename, 'res/' + foldername.split('/')[-1] + filename[5:])
 
 shutil.rmtree('extract')
+os.remove('copied_file.zip')
