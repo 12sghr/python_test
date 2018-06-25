f052e170dd24dce4034047272bcb860ba385e7b0
sghr12 <takezone103@gmail.com>
2018-06-25 17:45:45 +0900

    ファイル名を変更してresに格納

--- select_copy.py
+++ select_copy.py
@@ -3,5 +3,11 @@ import os, shutil, zipfile
 shutil.copy('101_ObjectCategories.zip', 'copied_file.zip')
 
 data = zipfile.ZipFile('copied_file.zip')
-data.extractall('test')
+data.extractall('extract')
 data.close()
+
+for foldername, subfolders, filenames in os.walk('extract/101_ObjectCategories'):
+    for filename in filenames:
+        shutil.move(foldername + '/' + filename, 'res/' + foldername.split('/')[-1] + filename[5:])
+
+shutil.rmtree('extract')
